05aa7f607e429128b5f1a181c0a5d048ffe7daf2
ash1k-dev <ash1kekb@yandex.ru>
2023-08-13 02:39:48 +0500

    Added get_games_id and get_steam_id

--- core/inventory/steam.py
+++ core/inventory/steam.py
@@ -1,12 +1,48 @@
+import os
+from random import randrange
+from time import sleep
+
 import requests
+from dotenv import load_dotenv
 from fake_useragent import UserAgent
+
+load_dotenv()
+
+apikey = os.getenv("APIKEY")
+user_id = os.getenv("USER_ID")
+
 
 ua = UserAgent()
 
 
+def get_games_id(user_id:str | int):
+    if type(user_id) is str:
+        user_id = get_steam_id(user_id)
+    url = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/"
+    games_id_full = requests.get(url, params={
+        'key': apikey,
+        'steamid': user_id,
+        'include_appinfo': 1,
+    })
+    games_id ={}
+    for i in games_id_full.json()['response']['games']:
+        games_id[i['appid']] = i['name']
+    return games_id
+
+
+def get_steam_id(user_id:str):
+    url = 'https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/'
+    user_id = requests.get(url, params={
+        'key': apikey,
+        'vanityurl': user_id,
+        'url_type': 1
+    })
+    return user_id
+
+
 def get_steam_inventory(user_id:int|str, game_id:int=730):
     if type(user_id) is str:
-        pass
+        user_id = get_steam_id(user_id)
     address = f'https://steamcommunity.com/inventory/{user_id}/{game_id}/2'
     data = requests.get(address)
     return data.json()
@@ -23,7 +59,7 @@ def get_classid_list(items:dict):
 def get_items_list(items:dict):
     market_names = {}
     for market_name in items["descriptions"]:
-        if market_name["type"]!="Extraordinary Collectible":
+        if market_name["type"]!="Extraordinary Collectible" and "Graffiti" not in market_name["market_hash_name"]:
             item = market_name["market_hash_name"]
             appid = market_name["appid"]
             classid = market_name["classid"]
@@ -41,11 +77,29 @@ def get_item_cost(name:str, game_id:int=730, currency:int=5):
     return market_item.json()['lowest_price']
 
 
-test_id = 76561198155948643
-
-test = get_steam_inventory(user_id=test_id)
+test = get_steam_inventory(user_id=user_id)
 items_list = get_items_list(items=test)
-inventory_cost = 0
-for item in items_list:
-    print(item, get_item_cost(item))
+test_items_list = {}
+test_inventory = []
+
+
+for k, item in enumerate(items_list):
+    test_inventory.append(item)
+
+for k, item in enumerate(items_list):
+    try:
+        test_items_list[item] = get_item_cost(item)
+        sleep(randrange(2, 10))
+    except BaseException:
+        pass
+
+
+print(test_inventory)
+print(len(test_inventory))
+
+print(test_items_list)
+print(len(test_items_list))
+
+print(set(test_inventory).difference(test_items_list.keys()))
+
 
