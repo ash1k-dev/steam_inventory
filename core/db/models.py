12b4df40842e6a0f817b26e1cdfd755ec9b43ce2
ash1k-dev <ash1kekb@yandex.ru>
2023-09-30 22:47:30 +0500

    Added alembic

--- core/db/models.py
+++ core/db/models.py
@@ -1,87 +1,95 @@
-from database import engine
-from sqlalchemy import Column, ForeignKey, Integer, SmallInteger, String
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import DeclarativeBase, declared_attr, relationship
 
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f"{cls.__name__.lower()}s"
+
+    id = Column(Integer, primary_key=True)
 
 
 class User(Base):
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True)
     user_name = Column(String, nullable=True)
     telegram_id = Column(Integer, nullable=False, unique=True)
-    # steam_id = Column(Integer, nullable=False)
 
-    steam_id = relationship('SteamId', back_populates='user')
+    steam_ids = relationship("SteamId", back_populates="user")
 
     def __repr__(self):
         return f"{self.__class__.__name__}, name: {self.user_name}, telegram_id: {self.telegram_id}"
 
 
 class SteamId(Base):
-    __tablename__ = 'steamids'
+    steam_id = Column(Integer)
+    steam_name = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
 
-    id = Column(Integer, primary_key=True)
-    steam_id = Column(Integer, nullable=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
-
-    user = relationship('User', back_populates='steamids')
-    steaminventory = relationship('SteamInventory', back_populates='steamids')
+    user = relationship("User", back_populates="steam_ids")
+    games = relationship("Game", back_populates="steamid")
+    # steam_inventorys = relationship("SteamInventory", back_populates="steamid")
 
     def __repr__(self):
         return f"{self.__class__.__name__}, steam_id: {self.steam_id}"
 
 
-class SteamInventory(Base):
-    __tablename__ = 'steam_inventorys'
+# class SteamInventory(Base):
+#     __tablename__ = "steam_inventorys"
+#
+#     id = Column(Integer, primary_key=True)
+#     games_id = Column(Integer, nullable=False)
+#     previous_inventory_cost = Column(Integer, default=0, nullable=False)
+#     now_inventory_cost = Column(Integer, nullable=False)
+#     steam_id = Column(Integer, ForeignKey("steamids.id"))
+#
+#     steamid = relationship("SteamId", back_populates="steam_inventorys")
+#     items_in = relationship(
+#         "SteamItem",
+#         secondary="steam_items_in_inventory",
+#         back_populates="steam_inventory",
+#     )
+#
+#     def __repr__(self):
+#         return f"{self.__class__.__name__}, games_id: {self.games_id}, cost: {self.now_inventory_cost}"
+#
+#
+# class SteamItem(Base):
+#     name = Column(String, nullable=False)
+#     app_id = Column(Integer, nullable=False)
+#     classid = Column(Integer, nullable=False)
+#     previous_item_cost = Column(Integer, default=0, nullable=False)
+#     now_item_cost = Column(Integer, nullable=False)
+#
+#     steam_inventory = relationship(
+#         "SteamInventory",
+#         secondary="steam_items_in_inventory",
+#         back_populates="items_in",
+#     )
+#
+#     def __repr__(self):
+#         return f"{self.__class__.__name__}, classid: {self.classid}, cost: {self.now_item_cost}"
+#
+#
+# class SteamItemsInInventory(Base):
+#     amount = Column(String())
+#     inventory_id = Column(Integer, ForeignKey("steam_inventorys.id"))
+#     item_id = Column(Integer, ForeignKey("steam_items.id"))
+#
+#     def __repr__(self):
+#         return (
+#             f"{self.__class__.__name__}, item_id: {self.item_id}, amount: {self.amount}"
+#         )
 
-    id = Column(Integer, primary_key=True)
-    games_id = Column(Integer, nullable=False)
-    previous_inventory_cost = Column(Integer, default=0, nullable=False)
-    now_inventory_cost = Column(Integer, nullable=False)
 
-    # items_quantity = Column(Integer, nullable=False)
-    # average_cost = Column(Integer, nullable=False)
-    # most_expensive_item = Column(Integer, nullable=False)
-    # most_cheaper_item = Column(Integer, nullable=False)
+class Game(Base):
+    game_name = Column(String)
+    game_id = Column(Integer)
+    game_cost = Column(Integer)
+    steam_id = Column(Integer, ForeignKey("steamids.id"))
 
-    steam_id = Column(Integer(), ForeignKey('steamids.id'))
-
-    steamid = relationship('SteamId', back_populates='steam_inventorys')
-    items_in = relationship("SteamItemsInInventory",
-                            secondary="steam_items_in_inventory",
-                            back_populates="steam_inventorys")
+    steamid = relationship("SteamId", back_populates="games")
 
     def __repr__(self):
-        return f"{self.__class__.__name__}, games_id: {self.games_id}, cost: {self.now_inventory_cost}"
-
-
-class SteamItem(Base):
-    __tablename__ = 'steam_items'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    app_id = Column(Integer, nullable=False)
-    classid = Column(Integer, nullable=False)
-    previous_item_cost = Column(Integer, default=0, nullable=False)
-    now_item_cost = Column(Integer, nullable=False)
-
-    def __repr__(self):
-        return f"{self.__class__.__name__}, classid: {self.classid}, cost: {self.now_item_cost}"
-
-
-class SteamItemsInInventory(Base):
-    __tablename__ = 'steam_items_in_inventory'
-
-    id = Column(Integer(), primary_key=True)
-    amount = Column(SmallInteger())
-    inventory_id = Column(Integer(), ForeignKey('steam_inventorys.id'))
-    item_id = Column(Integer(), ForeignKey('steam_items.id'))
-
-    def __repr__(self):
-        return f"{self.__class__.__name__}, item_id: {self.item_id}, amount: {self.amount}"
-
-
-Base.metadata.create_all(engine)
-# Base.metadata.drop_all(engine)
+        return f"{self.__class__.__name__}, item_id: {self.game_name}, amount: {self.game_cost}"
