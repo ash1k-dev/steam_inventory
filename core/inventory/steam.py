414aa039287e83d22b84dd12c55358a93fc52ad5
ash1k-dev <ash1kekb@yandex.ru>
2023-10-02 16:03:34 +0500

    Added def for get all games info

--- core/inventory/steam.py
+++ core/inventory/steam.py
@@ -13,18 +13,24 @@ def get_time_in_games(steam_id):
     """Getting time in all games"""
     url = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/"
     request = requests.get(url, params={"key": APIKEY, "steamid": steam_id})
-    games_list = request.json()["response"]
-    return games_list
-
-
-# print(get_time_in_games(76561198155948643))
+    games_list = request.json()["response"]["games"]
+    time_into_games = {}
+    for game in games_list:
+        if game["playtime_forever"] == 0:
+            time = 0
+        else:
+            time = round(game["playtime_forever"] / 60, 1)
+        time_into_games[game["appid"]] = time
+    return time_into_games
 
 
 def get_game_cost(game_id):
     """Getting game cost"""
     url = "http://store.steampowered.com/api/appdetails"
     request = requests.get(url, params={"appids": game_id, "cc": "ru"})
-    game_cost = request.json()[game_id]["data"]["price_overview"]["final_formatted"]
+    game_cost = request.json()[str(game_id)]["data"]["price_overview"][
+        "final_formatted"
+    ]
     game_cost = int(game_cost.split()[0])
     return game_cost
 
@@ -48,7 +54,7 @@ def get_steam_name(steam_id):
         url,
         params={
             "key": APIKEY,
-            "steamids": steam_id,
+            "steamids": int(steam_id),
         },
     )
     steam_name = request.json()["response"]["players"][0]["personaname"]
@@ -57,7 +63,6 @@ def get_steam_name(steam_id):
 
 def get_games_id(steam_id: str) -> dict:
     """Getting all games id"""
-    steam_id = get_steam_id(steam_id)
     url = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/"
     games_id_full = requests.get(
         url,
@@ -124,27 +129,16 @@ def get_item_cost(name: str, game_id: int = 730, currency: int = 5) -> float:
     return cost
 
 
-def all_test(steam_id: str):
+def get_all_games_info(steam_id):
+    final_list = {}
     games_id_list = get_games_id(steam_id)
-    for game in games_id_list:
-        steam_inventory = get_steam_inventory(user_id=int(steam_id), game_id=game)
-        items_list = get_items_list(items=steam_inventory)
-        classid_list = get_classid_list(items=steam_inventory)
-    steam_inventory = get_steam_inventory(user_id=int(steam_id), game_id=game)
-    steam_id = get_steam_id(steam_id)
-    classid_list = get_classid_list(items=steam_inventory)
-
-    steam_inventory = get_steam_inventory(user_id=steam_id)
-    items_list = get_items_list(items=steam_inventory)
-
-    test_items_list = {}
-    test_inventory_cost = 0
-    for k, item in enumerate(items_list):
+    time_into_games = get_time_in_games(steam_id)
+    for game_id, game_name in games_id_list.items():
+        time = time_into_games[game_id]
         try:
-            cost = get_item_cost(item)
-            test_items_list[item] = cost
-            test_inventory_cost += cost
-            sleep(randrange(3, 10))
-        except BaseException:
-            pass
-    return test_inventory_cost
+            price = get_game_cost(game_id)
+        except:
+            price = 0
+        final_list[game_id] = {"name": game_name, "time": time, "price": price}
+
+    return final_list
