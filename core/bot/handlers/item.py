0fc3ecb387b5613693dac2a0e33e1de9c1413bc3
ash1k-dev <ash1kekb@yandex.ru>
2023-10-20 03:56:52 +0500

    Refactored code(with ItemsCallbackFactory)

--- core/bot/handlers/item.py
+++ core/bot/handlers/item.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, F, Router
+from aiogram import Router
 from aiogram.types import CallbackQuery
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -10,66 +10,90 @@ from core.db.methods.request import (
 )
 
 
+from core.bot.keyboards.inline import ItemsCallbackFactory
+
+
 from core.bot.keyboards.inline import (
     get_items_menu,
+    get_pagination,
 )
 
 router = Router()
 
 
-@router.callback_query(F.data.startswith("inventory_"))
-async def show_info_for_items(callback: CallbackQuery, session: AsyncSession):
-    """Show info for current stream id"""
-    inventory_info_data = callback.data.split("_")
-    steamid_id = int(inventory_info_data[2])
+@router.callback_query(ItemsCallbackFactory.filter())
+async def test_change(
+    callback: CallbackQuery, callback_data: ItemsCallbackFactory, session: AsyncSession
+):
+    steam_id = callback_data.steam_id
+    steam_name = callback_data.steam_name
     items_info = await get_amount_and_items_info_from_db(
-        steam_id=steamid_id, session=session
+        steam_id=steam_id, session=session
     )
     total_cost = 0
     first_total_cost = 0
     amount = 0
     max_cost = 0
     min_cost = 10000000
-    all_items_dict = []
-    all_items_str = ""
-    for item in items_info:
+    all_items_list = []
+    for k, item in enumerate(items_info):
+        name = item.steam_item.name
         cost = item.amount * item.steam_item.item_cost
         first_cost = item.amount * item.steam_item.first_item_cost
         total_cost += cost
         first_total_cost += first_cost
+        store = f"https://steamcommunity.com/market/listings/730/{quote(item.steam_item.name)}"
         amount += 1
         if item.steam_item.item_cost > max_cost:
             max_cost = item.steam_item.item_cost
         if 0 < item.steam_item.item_cost < min_cost:
             min_cost = item.steam_item.item_cost
-        difference = item.steam_item.item_cost - item.steam_item.first_item_cost
-        all_items_dict.append(
-            {
-                "name": item.steam_item.name,
-                "cost": item.steam_item.item_cost,
-                "first_cost": item.steam_item.first_item_cost,
-                "diff": difference,
-                "store": f"https://steamcommunity.com/market/listings/730/{quote(item.steam_item.name)}",
-            }
+        diff = item.steam_item.item_cost - item.steam_item.first_item_cost
+        if first_cost != 0:
+            all_items_list.append(
+                f"{name}\n"
+                f"Первоначальная стоимость: {first_cost}руб.\n"
+                f"Текущая стоимость предмета: {cost}руб.\n"
+                f"Прирост стоимости: {diff}руб.({int(diff/first_cost*100)}%)\n"
+                f"Ссылка на торговую площадку: {store}\n\n"
+            )
+    difference_total_cost = total_cost - first_total_cost
+    if callback_data.action == "all":
+        await callback.message.edit_text(
+            text=f"{all_items_list[callback_data.page]}",
+            disable_webpage_preview=True,
+            reply_markup=get_pagination(
+                action="all",
+                page=callback_data.page,
+                pages_amount=callback_data.pages_amount,
+                steam_id=callback_data.steam_id,
+                steam_name=callback_data.steam_name,
+            ),
         )
-    for data in all_items_dict[0:5]:
-        all_items_str += f'{data["name"]}\nПервоначальная стоимость:{data["first_cost"]}\nТекущая стоимость предмета {data["cost"]}\nПрирост стоимости {data["diff"]}\n Ссылка на торговую площадку:{data["store"]}\n\n'
-
-    await callback.message.answer(
-        text=f"Количество предметов на аккаунте: {all_items_str}\nОбщая стоимость предметов на аккаунте: {total_cost} \nПервоначальная стоимость предметов на аккаунте: {first_total_cost}\nМаксимальная стоимость предмета: {max_cost} \nМинимальная стоимость предмета: {min_cost}",
-        reply_markup=get_items_menu(steam_id=steamid_id),
-    )
-    await callback.answer()
-
-
-# @router.callback_query(F.data.startswith("items_list_"))
-# async def show_info_for_items(callback: CallbackQuery, session: AsyncSession):
-#     """Show info for current stream id"""
-#     inventory_info_data = callback.data.split("_")
-#     steamid_id = int(inventory_info_data[3])
-#     items_info = await get_items_info_from_db(steam_id=steamid_id, session=session)
-#     await callback.message.answer(
-#         text=f"Количество предметов на аккаунте: {items_info}",
-#         reply_markup=get_items_menu(steam_id=steamid_id),
-#     )
-#     await callback.answer()
+    elif callback_data.action == "top":
+        await callback.message.edit_text(
+            text=f"{all_items_list[callback_data.page]}",
+            disable_webpage_preview=True,
+            reply_markup=get_pagination(
+                action="top",
+                page=callback_data.page,
+                pages_amount=callback_data.pages_amount,
+                steam_id=callback_data.steam_id,
+                steam_name=callback_data.steam_name,
+            ),
+        )
+    elif callback_data.action == "info" or callback_data.action == "back":
+        await callback.message.answer(
+            text=f"Аккаунт {steam_name}\n"
+            f"Количество предметов на аккаунте: {amount}\n"
+            f"Общая стоимость предметов на аккаунте: {total_cost}руб.\n"
+            f"Первоначальная стоимость предметов на аккаунте: {first_total_cost}руб.\n"
+            f"Прирост стоимости: {difference_total_cost}руб.({int((difference_total_cost/first_total_cost)*100)}%)\n"
+            f"Максимальная стоимость предмета: {max_cost} \n"
+            f"Минимальная стоимость предмета: {min_cost}",
+            disable_webpage_preview=True,
+            reply_markup=get_items_menu(
+                steam_id=callback_data.steam_id,
+                steam_name=callback_data.steam_name,
+            ),
+        )
