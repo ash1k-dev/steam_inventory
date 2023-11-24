1b1c6d4da85f4213c7f5b639b972eb8c558d06d7
ash1k-dev <ash1kekb@yandex.ru>
2023-11-24 21:02:18 +0500

    Deleted unnecessary

--- core/bot/keyboards/reply.py
+++ core/bot/keyboards/reply.py
@@ -7,44 +7,15 @@ def get_main_menu() -> ReplyKeyboardMarkup:
     keyboard_builder.button(text="Мои Steam id")
     keyboard_builder.button(text="Отслеживание стоимости")
     keyboard_builder.button(text="Помощь")
-    keyboard_builder.adjust(1)
-    return keyboard_builder.as_markup(resize_keyboard=True, one_time_keyboard=True)
+    keyboard_builder.adjust(2, 1)
+    return keyboard_builder.as_markup(resize_keyboard=True)
 
 
-
-def get_items_and_games_menu() -> ReplyKeyboardMarkup:
-    """Items and games keyboard"""
-    keyboard_builder = ReplyKeyboardBuilder()
-    keyboard_builder.button(text="Предметы")
-    keyboard_builder.button(text="Игры")
-    keyboard_builder.button(text="Назад")
-    keyboard_builder.adjust(1)
-    return keyboard_builder.as_markup(resize_keyboard=True, one_time_keyboard=True)
-
-def get_items_menu() -> ReplyKeyboardMarkup:
-    """Items keyboard"""
-    keyboard_builder = ReplyKeyboardBuilder()
-    keyboard_builder.button(text="Текущая стоимость скинов")
-    keyboard_builder.button(text="Динамика изменения")
-    keyboard_builder.button(text="Назад")
-    keyboard_builder.adjust(1)
-    return keyboard_builder.as_markup(resize_keyboard=True, one_time_keyboard=True)
-
-def get_games_menu() -> ReplyKeyboardMarkup:
-    """Games keyboard"""
-    keyboard_builder = ReplyKeyboardBuilder()
-    keyboard_builder.button(text="Текущая стоимость игр")
-    keyboard_builder.button(text="Время в играх")
-    keyboard_builder.button(text="Назад")
-    keyboard_builder.adjust(1)
-    return keyboard_builder.as_markup(resize_keyboard=True, one_time_keyboard=True)
-
-
-
-def get_check_cost_menu() -> ReplyKeyboardMarkup:
+def get_track_menu() -> ReplyKeyboardMarkup:
     """Сheck cost keyboard"""
     keyboard_builder = ReplyKeyboardBuilder()
     keyboard_builder.button(text="Предметы")
     keyboard_builder.button(text="Игры")
-    keyboard_builder.adjust(1)
-    return keyboard_builder.as_markup(resize_keyboard=True, one_time_keyboard=True)
+    keyboard_builder.button(text="Назад")
+    keyboard_builder.adjust(2)
+    return keyboard_builder.as_markup(resize_keyboard=True)
