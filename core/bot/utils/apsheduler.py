7862351c954a4f0edd6cd81e384a3edd08be6982
ash1k-dev <ash1kekb@yandex.ru>
2023-11-30 01:41:36 +0500

    Add scheduler logic

---
+++ core/bot/utils/apsheduler.py
@@ -0,0 +1,110 @@
+from aiogram import Bot
+from aiogram.utils import markdown
+from sqlalchemy.ext.asyncio import async_sessionmaker
+
+from methods.request import (
+    get_all_tracking_items_from_db,
+    get_all_tracking_games_from_db,
+    get_all_user_from_db,
+    get_all_steam_ids_from_db,
+    get_amount_and_items_info_from_db,
+)
+
+from urllib.parse import quote
+
+
+from methods.update import update_all_items, update_all_games
+
+
+async def check_update(bot: Bot, sessionmaker: async_sessionmaker) -> None:
+    async with sessionmaker() as session:
+        await update_all_items(session=session)
+        await update_all_games(session=session)
+        all_users = await get_all_user_from_db(session=session)
+        for user in all_users:
+            await get_items_changes(
+                bot=bot, user_telegram_id=user.telegram_id, session=session
+            )
+            await get_tracking_items_changes(
+                bot=bot, user_telegram_id=user.telegram_id, session=session
+            )
+            await get_tracking_game_changes(
+                bot=bot, user_telegram_id=user.telegram_id, session=session
+            )
+
+
+async def get_items_changes(bot, session, user_telegram_id):
+    all_steam_ids = await get_all_steam_ids_from_db(
+        telegram_id=user_telegram_id, session=session
+    )
+    for i in all_steam_ids:
+        for steam_id in all_steam_ids:
+            items = f"Подорожавшие предметы на аккаунте {steam_id.steam_name}:\n\n"
+            items_info = await get_amount_and_items_info_from_db(
+                steam_id=steam_id.id, session=session
+            )
+            for item in items_info:
+                name, item_cost, first_item_cost, _ = item
+                difference_for_item = item_cost - first_item_cost
+                if item_cost > first_item_cost * 1.4:
+                    items += (
+                        f"Название предмета: {name} \nПервоначальная стоимость: {first_item_cost} \n"
+                        f"Актуальная стоимость: {item_cost} \n"
+                        f"Прирост: +{difference_for_item} руб. (+{int((difference_for_item / first_item_cost) * 100)}%)\n"
+                        f"Ссылка на предмет: {markdown.hlink('SteamLink', f'https://steamcommunity.com/market/listings/730/{quote(name)}')} \n\n\n"
+                    )
+            if items != f"Подорожавшие предметы на аккаунте {steam_id.steam_name}:\n\n":
+                await bot.send_message(
+                    chat_id=user_telegram_id,
+                    disable_web_page_preview=True,
+                    text=items,
+                )
+
+
+async def get_tracking_items_changes(bot, session, user_telegram_id):
+    all_tracking_items = await get_all_tracking_items_from_db(
+        telegram_id=user_telegram_id, session=session
+    )
+    tracking_items = "Подешевевшие предметы:\n\n"
+    for tracking_item in all_tracking_items:
+        name, first_item_cost, _, item_id, item_cost = tracking_item
+        difference_for_tracking_items = item_cost - first_item_cost
+        if item_cost < first_item_cost * 0.6:
+            tracking_items += (
+                f"Название предмета: {name} \n"
+                f"Первоначальная стоимость: {first_item_cost} \n"
+                f"Актуальная стоимость: {item_cost} \n"
+                f"Понижение: -{difference_for_tracking_items} руб.(-{int((difference_for_tracking_items / first_item_cost) * 100)}%) \n"
+                f"Ссылка на предмет: {markdown.hlink('SteamLink', f'https://steamcommunity.com/market/listings/730/{quote(name)}')} \n\n\n"
+            )
+    if tracking_items != "Подешевевшие предметы:\n\n":
+        await bot.send_message(
+            chat_id=user_telegram_id,
+            disable_web_page_preview=True,
+            text=tracking_items,
+        )
+
+
+async def get_tracking_game_changes(bot, user_telegram_id, session):
+    all_tracking_games = await get_all_tracking_games_from_db(
+        telegram_id=user_telegram_id, session=session
+    )
+    tracking_games = "Подешевевшие игры:\n\n"
+    for tracking_game in all_tracking_games:
+        _, name, game_id, first_game_cost, game_cost = tracking_game
+        difference_for_tracking_games = first_game_cost - game_cost
+        if difference_for_tracking_games > first_game_cost * 0.4:
+            tracking_games += (
+                f"Название игры: {name} \n"
+                f"Первоначальная стоимость: {first_game_cost} \n"
+                f"Актуальная стоимость: {game_cost} \n"
+                f"Изменение: -{difference_for_tracking_games} руб.(-{int((difference_for_tracking_games / first_game_cost) * 100)}%) \n"
+                f"Ссылка на игру: "
+                f"{markdown.hlink('SteamLink', f'https://store.steampowered.com/app/{game_id}')}\n\n"
+            )
+    if tracking_games != "Подешевевшие игры:\n\n":
+        await bot.send_message(
+            chat_id=user_telegram_id,
+            disable_web_page_preview=True,
+            text=tracking_games,
+        )
--- main.py
+++ main.py
@@ -2,15 +2,17 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from aiogram.fsm.storage.redis import RedisStorage
 
 
 from config import DB_URL, TOKEN, REDIS_URL
 from core.bot.handlers import user, game, item, track
+from core.bot.utils import admin_notification
 from core.bot.middlewares.db_connection import DbConnection
 from core.bot.middlewares.long_operation import LongOperationMiddleware
-from core.bot.utils import admin_notification
+from core.bot.utils.apsheduler import check_update
 
 
 async def start():
@@ -27,6 +29,15 @@ async def start():
 
     engine = create_async_engine(url=DB_URL, echo=True)
     sessionmaker = async_sessionmaker(engine, expire_on_commit=False)
+
+    sheduler = AsyncIOScheduler(timezone="Europe/Moscow")
+    sheduler.add_job(
+        check_update,
+        trigger="interval",
+        seconds=600,
+        kwargs={"bot": bot, "sessionmaker": sessionmaker},
+    )
+    sheduler.start()
 
     dp.update.middleware(LongOperationMiddleware())
     dp.update.middleware(DbConnection(sessionmaker))
