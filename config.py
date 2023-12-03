ad56c4936b1c7f57f9b64077e7559be02ad5b499
ash1k-dev <ash1kekb@yandex.ru>
2023-12-03 06:03:41 +0500

    Add constants for updates

--- config.py
+++ config.py
@@ -8,9 +8,11 @@ load_dotenv()
 DB_URL = os.getenv("DB_URL")
 APIKEY = os.getenv("APIKEY")
 USER_ID = os.getenv("USER_ID")
+REDIS_URL = os.getenv("REDIS_URL")
 
 
 TOKEN = os.getenv("TOKEN")
 ADMIN_ID = os.getenv("ADMIN_ID")
 
-
+DEPRECIATION_FACTOR = os.getenv("DEPRECIATION_FACTOR")
+INCREASE_FACTOR = os.getenv("INCREASE_FACTOR")
